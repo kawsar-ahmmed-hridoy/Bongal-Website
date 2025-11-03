1f6db4f7d9c6a5ddb7313df9cb833289c4c3f754
kawsar_ahmmed_hridoy <kawsarhridoy0146@gmail.com>
2025-11-04 01:50:20 +0600

    added loading animation

---
+++ bongal-client/src/components/common/Loader.jsx
@@ -0,0 +1,14 @@
+import { Package } from 'lucide-react';
+
+const Loader = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="text-center">
+        <Package className="w-16 h-16 text-green-600 animate-bounce mx-auto mb-4" />
+        <p className="text-gray-600 text-lg">Loading...</p>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
