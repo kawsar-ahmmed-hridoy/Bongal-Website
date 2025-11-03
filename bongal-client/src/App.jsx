3bd71fcd738a6b0e6f8e329d0003571df54aa0c0
kawsar_ahmmed_hridoy <kawsarhridoy0146@gmail.com>
2025-11-04 02:35:51 +0600

    added all the navigation

--- bongal-client/src/App.jsx
+++ bongal-client/src/App.jsx
@@ -1,19 +1,53 @@
-import { Routes, Route } from "react-router-dom";
-import Layout from "./components/common/Layout";
-import HomePage from "./components/home/HomePage";
-import NotFound from "./components/common/NotFound";
-import LoginPage from "./components/auth/LoginPage";
-import RegisterPage from "./components/auth/RegisterPage";
+import { Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import { CartProvider } from './context/CartContext';
+import Layout from './components/common/Layout';
+import HomePage from './components/home/HomePage';
+import ProductsPage from './components/products/ProductsPage';
+import ProductDetail from './components/products/ProductDetail';
+import CartPage from './components/cart/CartPage';
+import OrdersPage from './components/orders/OrdersPage';
+import LoginPage from './components/auth/LoginPage';
+import RegisterPage from './components/auth/RegisterPage';
+import AdminDashboard from './components/admin/AdminDashboard';
+import ProtectedRoute from './components/common/ProtectedRoute';
+import NotFound from './components/common/NotFound';
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<Layout />} />
-        <Route index element={<HomePage />} />
-        <Route path="login" element={<LoginPage />} />
-        <Route path="register" element={<RegisterPage />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+    <AuthProvider>
+      <CartProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="products" element={<ProductsPage />} />
+            <Route path="products/:id" element={<ProductDetail />} />
+            <Route path="cart" element={<CartPage />} />
+            <Route path="login" element={<LoginPage />} />
+            <Route path="register" element={<RegisterPage />} />
+
+            <Route
+              path="orders"
+              element={
+                <ProtectedRoute>
+                  <OrdersPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="admin/*"
+              element={
+                <ProtectedRoute adminOnly>
+                  <AdminDashboard />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </CartProvider>
+    </AuthProvider>
   );
 }
 
