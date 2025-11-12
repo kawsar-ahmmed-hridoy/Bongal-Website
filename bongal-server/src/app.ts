62c8d8baa2cf517be3a88de3d4702b3db0eaf1db
kawsar_ahmmed_hridoy <kawsarhridoy0146@gmail.com>
2025-11-12 10:51:57 +0600

    fixed frontend url

--- bongal-server/src/app.ts
+++ bongal-server/src/app.ts
@@ -13,7 +13,7 @@ dotenv.config();
 const app: Express = express();
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true,
 }));
 app.use(express.json());
